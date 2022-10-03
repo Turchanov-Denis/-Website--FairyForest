108e864568d1c06717ac0d08422be649cbb4c72f
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-04 00:29:14 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,4 @@
 window.addEventListener("scroll", event=>{
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
 })
 
-gsap.registerPugin(ScrollTrigger,ScrollSmoother);
-
-ScrollSmoother.create({
-    wrapper: ".wrapper",
-    container: ".container"
-})
